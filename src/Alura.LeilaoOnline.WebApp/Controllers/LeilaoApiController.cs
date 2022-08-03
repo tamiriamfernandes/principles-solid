d48f48a8da205eacbda5d566d894294db4852f02
Tamiriam Fernandes <tamiriam.fernandes@copastur.com.br>
2022-08-03 17:34:08 -0300

    criado a class dao - principio de responsabilidade unica

---
+++ src/Alura.LeilaoOnline.WebApp/Controllers/LeilaoApiController.cs
@@ -0,0 +1,64 @@
+﻿using Alura.LeilaoOnline.WebApp.Dados;
+using Alura.LeilaoOnline.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alura.LeilaoOnline.WebApp.Controllers
+{
+    [ApiController]
+    [Route("/api/leiloes")]
+    public class LeilaoApiController : ControllerBase
+    {
+        LeilaoDao _dao;
+
+        public LeilaoApiController()
+        {
+            _dao = new LeilaoDao();
+        }
+
+        [HttpGet]
+        public IActionResult EndpointGetLeiloes()
+        {
+            var leiloes = _dao.BuscarLeiloes();
+            return Ok(leiloes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult EndpointGetLeilaoById(int id)
+        {
+            var leilao = _dao.BuscarPorId(id);
+            if (leilao == null)
+            {
+                return NotFound();
+            }
+            return Ok(leilao);
+        }
+
+        [HttpPost]
+        public IActionResult EndpointPostLeilao(Leilao leilao)
+        {
+            _dao.Incluir(leilao);
+            return Ok(leilao);
+        }
+
+        [HttpPut]
+        public IActionResult EndpointPutLeilao(Leilao leilao)
+        {
+            _dao.Alterar(leilao);
+            return Ok(leilao);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult EndpointDeleteLeilao(int id)
+        {
+            var leilao = _dao.BuscarPorId(id);
+            if (leilao == null)
+            {
+                return NotFound();
+            }
+            _dao.Excluir(leilao);
+            return NoContent();
+        }
+
+
+    }
+}
