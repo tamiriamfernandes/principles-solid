6965d8cf5d4bea8ad20938a6ea35599d5252a52c
Tamiriam Fernandes <tamiriam.fernandes@copastur.com.br>
2022-08-10 23:31:47 -0300

    principio aberto/fechado - criado novas classes onde a aplicação fica fechada para alterações e aberta para extensoes.

---
+++ src/Alura.LeilaoOnline.WebApp/Services/IAdmService.cs
@@ -0,0 +1,19 @@
+﻿using Alura.LeilaoOnline.WebApp.Models;
+using System.Collections.Generic;
+
+namespace Alura.LeilaoOnline.WebApp.Services
+{
+    public interface IAdmService
+    {
+        IEnumerable<Categoria> ConsultaCategorias();
+        IEnumerable<Leilao> ConsultaLeiloes();
+        Leilao ConsultaLeilaoPorId(int id);
+        void CadastraLeilao(Leilao leilao);
+        void ModificaLeilao(Leilao leilao);
+        void RemoveLeilao(Leilao leilao);
+        void IniciaPregaoDoLeilaoComId(int id);
+        void FinalizaPregaoDoLeilaoComID(int id);
+
+
+    }
+}
